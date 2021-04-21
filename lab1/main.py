5c21a4cf576d2d0586fd28fc5fb7228177a39216
penobulyator <dima.onishuk70@gmail.com>
2021-04-21 23:53:23 +0700

    Fixing gain_ratio

--- lab1/main.py
+++ lab1/main.py
@@ -1,9 +1,13 @@
 import csv
+import itertools
+
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+from collections import Counter
+from sklearn.feature_selection import mutual_info_classif
 
 
 def trim(values):
@@ -24,7 +28,7 @@ def fill_gaps(values):
         avg = np.nanmean(values)
 
         for i in range(0, len(values) - 1):
-            if values[i] is np.nan:
+            if np.isnan(values[i]):
                 values[i] = avg
 
 
@@ -85,9 +89,19 @@ def splitter(values, n):
     return out
 
 
+# [1, 1, 2, 2, 3, 3, 3, nan] -> [1, 2, 3, nan]
 def split_column(column):
-    # TODO
-    pass
+    counter = Counter(column)
+    result = {}
+    for key, value in counter.items():
+        if np.isnan(key):
+            if np.nan in result.keys():
+                result[np.nan] += 1
+            else:
+                result[np.nan] = 1
+        else:
+            result[key] = value
+    return result
 
 
 def get_classes(dataframe, targets, groups_count):
@@ -99,10 +113,10 @@ def get_classes(dataframe, targets, groups_count):
         if groups_count[i] == -1:
             # divide by unique values
 
-            target_unique_values = np.unique(dataframe[target][~np.isnan(dataframe[target])])
+            target_unique_values = split_column(dataframe[target].tolist()).keys()
             for value in target_unique_values:
-                if value is np.nan:
-                    intervals.append(np.nan)
+                if np.isnan(value):
+                    intervals.append(value)
                 else:
                     intervals.append([value, value])
         else:
@@ -114,12 +128,10 @@ def get_classes(dataframe, targets, groups_count):
         targets_groups.append(intervals)
 
     classes = []
-
-    for i in range(len(targets_groups[0])):
-        for j in range(len(targets_groups[1])):
+    for elem in itertools.product(targets_groups[0], targets_groups[1]):
             new_class = {
-                targets[0]: targets_groups[0][i],
-                targets[1]: targets_groups[1][j]
+                targets[0]: elem[0],
+                targets[1]: elem[1]
             }
             classes.append(new_class)
 
@@ -131,11 +143,11 @@ def row_belongs_to_class(dataframe, row_index, gain_ratio_class):
         row_target_value = dataframe[target][row_index]
         value_range = gain_ratio_class[target]
 
-        if value_range is np.nan:
-            if row_target_value is not np.nan:
+        if not isinstance(value_range, list):
+            if not np.isnan(row_target_value):
                 return False
         else:
-            if row_target_value is np.nan:
+            if np.isnan(row_target_value):
                 return False
             elif not value_range[0] <= row_target_value <= value_range[1]:
                 return False
@@ -143,36 +155,36 @@ def row_belongs_to_class(dataframe, row_index, gain_ratio_class):
     return True
 
 
-def gain_ratio(dataframe):
+def get_row_classes(dataframe, classes):
+    row_classes = []
+
+    for row_number, index in enumerate(dataframe.index):
+        has_class = False
+        for i, _class in enumerate(classes):
+            if row_belongs_to_class(dataframe, index, _class):
+                row_classes.append(i)
+                has_class = True
+                break
+        if not has_class:
+            print("Class is unknown for row number " + str(row_number))
+
+    return row_classes
+
+
+def print_gain_ratio(dataframe):
     targets = ['КГФ', 'G_total']
     kgf_groups_count = 1 + int(math.log(len(dataframe['КГФ']), 2))
     classes = get_classes(dataframe, targets, [kgf_groups_count, -1])
 
-    for column in dataframe.columns:
-        T = split_column(column)
-        # TODO
+    row_classes = get_row_classes(dataframe, classes)
 
+    mic = mutual_info_classif(dataframe[targets].fillna(0), row_classes)
+    mic.index = dataframe[targets].columns
+    mic.sort_values(ascending=False)
 
-def main():
-    # read dataframe from file
-    with open('dataset.csv', newline='') as csv_file:
-        dataframe = read_csv(csv_file)
+    print(mic)
 
-    # print gain ratios
-    gain_ratio(dataframe)
-
-    # build heatmap
-    corr_matrix = dataframe.corr()
-    analyze_correlations(corr_matrix)
-    # sns.heatmap(corr_matrix)
-
-    # build histograms
-    # plt.rc('figure', max_open_warning=0)
-    # for name in dataframe.columns:
-    #     pd.DataFrame(dataframe[name]).hist()
-
-    # show
-    plt.show()
+    return
 
 
 def get_classes_test():
@@ -188,6 +200,33 @@ def get_classes_test():
         print("x: {}".format(str(_class['x'])))
         print("y: {}".format(str(_class['y'])))
         print("")
+
+    row_classes = get_row_classes(dataframe, test)
+
+    for i, class_number in enumerate(row_classes):
+        print("Row {} belongs to class {}".format(i, class_number))
+
+
+def main():
+    # read dataframe from file
+    with open('dataset.csv', newline='') as csv_file:
+        dataframe = read_csv(csv_file)
+
+    # print gain ratios
+    print_gain_ratio(dataframe)
+
+    # build heatmap
+    corr_matrix = dataframe.corr()
+    analyze_correlations(corr_matrix)
+    # sns.heatmap(corr_matrix)
+
+    # build histograms
+    # plt.rc('figure', max_open_warning=0)
+    # for name in dataframe.columns:
+    #     pd.DataFrame(dataframe[name]).hist()
+
+    # show
+    plt.show()
 
 
 if __name__ == "__main__":
